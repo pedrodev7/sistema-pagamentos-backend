c3bcfc481ef1c4f7ff431e8ce1b397707e7ca2eb
Pedro Henrique <pedrohenrique096k@gmail.com>
2026-06-23 21:48:09 -0300

    commit inicial do projeto

---
+++ ms-transacoes/src/main/java/com/projeto/ms_transacoes/MsTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ms_transacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransacoesApplication.class, args);
+	}
+
+}
---
+++ ms-transacoes/src/test/java/com/projeto/ms_transacoes/MsTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ms_transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
